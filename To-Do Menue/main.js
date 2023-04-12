d17a46740658712a31c650c7e68f177774d765e7
Corsin <corsin.pollo@gmail.com>
2023-04-12 13:08:42 +0200

    getTasks updated

--- To-Do Menue/main.js
+++ To-Do Menue/main.js
@@ -17,7 +17,10 @@ InputElement.addEventListener("input", () => {
 Add.addEventListener("click", () => {
   let Input = InputElement.value;
   let NewObject = document.createElement("li");
-  NewObject.innerText = Input;
+  const newTaskElementInput = document.createElement("input")
+  newTaskElementInput.value = Input;
+  newTaskElementInput.readOnly = true
+  NewObject.append(newTaskElementInput)
   let edit_button = document.createElement("button");
   edit_button.title = "Bearbeiten";
   edit_button.innerText = "Bearbeiten";
@@ -35,43 +38,23 @@ Add.addEventListener("click", () => {
     //alert("Erfolgreich gelöscht")
   });
 
-  const tasksElement = document.getElementById("tasks");
 
-  async function getTasks() {
-    const response = await fetch("http://localhost:3000/tasks", {
-      method: "GET"
-    })
-    const json = await response.json()
-
-    renderTasks(json)
-  }
-
-  function renderTasks(tasks) {
-    tasksElement.replaceChildren();
-
-    Array.from(tasks).forEach(function (item) {
-      const liElement = document.createElement("li")
-      liElement.innerText = item.title;
-      tasksElement.append(liElement)
-    })
-  }
 
   //Das Inputfeld wird geleert
   InputElement.value = "";
   Add.disabled = true;
 
   edit_button.addEventListener("click", () => {
+    //debugger
     document.getElementById("edit_div").style.display = "block";
-    document.getElementById("edit_input").value = NewObject.innerText.split("x")[0].trim();
+    document.getElementById("edit_input").value = newTaskElementInput.value.trim();
     let Save = document.getElementById("Save");
     let x = NewObject;
     
 
     Save.onclick = () => {
       let UpdatedText = document.getElementById("edit_input").value;
-      x.innerText = UpdatedText;
-      x.appendChild(delete_button);
-      x.appendChild(edit_button);
+      newTaskElementInput.value = UpdatedText;
       document.getElementById("edit_div").style.display = "none";
     };
   });
@@ -85,3 +68,62 @@ Add.addEventListener("click", () => {
     };
 };*/
 
+const tasksElement = document.getElementById("tasks");
+
+async function getTasks() {
+  const response = await fetch("http://localhost:3000/tasks", {
+    method: "GET"
+  })
+  const json = await response.json()
+
+  renderTasks(json)
+}
+
+function renderTasks(tasks) {
+
+  const tasksElement = document.getElementById("todo_ul");
+  tasksElement.replaceChildren();
+
+  Array.from(tasks).forEach(function (item) {
+    const newTaskElement = document.createElement("li");
+    const newTaskElementInput = document.createElement("input")
+    newTaskElementInput.value = item.title;
+    newTaskElementInput.readOnly = true
+    newTaskElement.append(newTaskElementInput)
+
+    const taskList = document.getElementById("todo_ul");
+    taskList.append(newTaskElement);
+
+    const edit_button = document.createElement("button");
+    edit_button.title = "Bearbeiten";
+    edit_button.innerText = "Bearbeiten";
+    newTaskElement.appendChild(edit_button);
+
+    const delete_button = document.createElement("button");
+    delete_button.innerText = "x";
+    newTaskElement.appendChild(delete_button);
+    
+    todo_ul.appendChild(newTaskElement);
+
+    delete_button.addEventListener("click", () => {
+      newTaskElement.remove();
+      //alert("Erfolgreich gelöscht")
+    });
+
+    edit_button.addEventListener("click", () => {
+      //debugger
+      document.getElementById("edit_div").style.display = "block";
+      document.getElementById("edit_input").value = newTaskElementInput.value.trim();
+      const Save = document.getElementById("Save");
+      
+
+      Save.onclick = () => {
+        const updatedText = document.getElementById("edit_input").value;
+        newTaskElementInput.value = updatedText;
+        document.getElementById("edit_div").style.display = "none";
+      };
+    });
+  })
+}
+
+getTasks();
